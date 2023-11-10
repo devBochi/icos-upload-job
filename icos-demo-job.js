e69c64cc8ca94988a3031c313eb316d5e5799fac
Nahuel Montes de Oca <88986660+devBochi@users.noreply.github.com>
2023-11-10 16:40:42 -0300

    Add files via upload

--- icos-demo-job.js
+++ icos-demo-job.js
@@ -1,31 +1,22 @@
-// var ibm = require('ibm-cos-sdk');
-// var util = require('util');
+var ibm = require('ibm-cos-sdk');
 
-/*
 var config = {
-    endpoint: '<endpoint>',
-    apiKeyId: '<api-key>',
-    serviceInstanceId: '<resource-instance-id>',
+    endpoint: process.env.ENDPOINT,
+    apiKeyId: process.env.API_KEY,
+    serviceInstanceId: process.env.SERV_ID,
 };
-*/
-
-// var cos = new ibm.S3(config);
+var cos = new ibm.S3(config);
 
 const evento = JSON.parse(process.env.CE_DATA)
 
-// APIKEY=VamGabdPmuB_7FNxecCb1yvARlwCVXD7Su_ft67N0rEP
-// ICOS(public)=s3.us-south.cloud-object-storage.appdomain.cloud
-
 let params = {
     Bucket: evento.bucket, 
-    Key: evento.object
+    Key: evento.key
 };
 
 console.log(params)   
 
-/*
 cos.getObject(params, function(err, data) {
      if (err) console.log(err, err.stack); // an error occurred
      else     console.log(data);           // successful response
-   });
-*/
+});
