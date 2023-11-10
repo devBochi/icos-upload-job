798fad79fd1579ec77553d95482603f066c8f727
Nahuel Montes de Oca <nahu.mdoca@ibm.com>
2023-11-10 20:31:55 -0300

    Add comments and fixes

--- icos-demo-job.js
+++ icos-demo-job.js
@@ -1,5 +1,6 @@
 var ibm = require('ibm-cos-sdk');
 
+// Credentials for ICOS sdk
 var config = {
     endpoint: process.env.ENDPOINT,
     apiKeyId: process.env.API_KEY,
@@ -7,15 +8,17 @@ var config = {
 };
 var cos = new ibm.S3(config);
 
-const evento = JSON.parse(process.env.CE_DATA)
+// We parse the metadata that comes from the ICOS put event
+const eventInfo = JSON.parse(process.env.CE_DATA)
 
 let params = {
-    Bucket: evento.bucket, 
-    Key: evento.key
+    Bucket: eventInfo.bucket, 
+    Key: eventInfo.key
 };
 
 console.log(params)   
 
+// With the info of the event, we can read the object and do whatever we want
 cos.getObject(params, function(err, data) {
      if (err) console.log(err, err.stack); // an error occurred
      else     console.log(data);           // successful response
