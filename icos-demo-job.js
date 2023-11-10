d5c342ae779f8a9f5039598f955837b9f1e4f0ef
Nahuel Montes de Oca <88986660+devBochi@users.noreply.github.com>
2023-11-10 15:47:59 -0300

    Add files via upload

--- icos-demo-job.js
+++ icos-demo-job.js
@@ -11,25 +11,14 @@ var config = {
 
 // var cos = new ibm.S3(config);
 
-console.log(JSON.parse(process.env.CE_DATA))
-
-console.log(process.env.CE_DATA.notification)
-
-let event = {}
-
-event = process.env.CE_DATA
+const evento = JSON.parse(process.env.CE_DATA)
 
 // APIKEY=VamGabdPmuB_7FNxecCb1yvARlwCVXD7Su_ft67N0rEP
 // ICOS(public)=s3.us-south.cloud-object-storage.appdomain.cloud
 
-const eventInfo = {
-    object: event.key,
-    bucket: event.bucket
-}
-
-var params = {
-    Bucket: eventInfo.bucket, 
-    Key: eventInfo.object
+let params = {
+    Bucket: evento.bucket, 
+    Key: evento.object
 };
 
 console.log(params)   
